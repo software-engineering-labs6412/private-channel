e2a001dc2318ca5e39d61646c33782ceda96b30d
Василий Родин <varodg2000@mail.ru>
2022-03-09 23:05:08 +0300

    добавил контроллер принимающий расписание

--- client/src/main/java/org/ssau/privatechannel/controller/ScheduleController.java
+++ client/src/main/java/org/ssau/privatechannel/controller/ScheduleController.java
@@ -2,27 +2,65 @@ package org.ssau.privatechannel.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.ssau.privatechannel.comparator.TimeFrameComparatorToMax;
+import org.ssau.privatechannel.comparator.TimeFrameComparatorToMin;
+import org.ssau.privatechannel.firetasks.EndDataTransferringTask;
+import org.ssau.privatechannel.firetasks.StartDataTransferringTask;
 import org.ssau.privatechannel.model.Schedule;
+import org.ssau.privatechannel.model.TimeFrame;
 import org.ssau.privatechannel.repository.ScheduleRepository;
+import org.ssau.privatechannel.service.ConfidentialInfoService;
+import org.ssau.privatechannel.service.TimerService;
+
+import java.time.LocalDateTime;
+import java.util.Collection;
+import java.util.Optional;
 
 @RestController
-@RequestMapping(path = "/schedule")
+@RequestMapping(path = "/api/v1")
 public class ScheduleController {
 
+    private final ScheduleRepository scheduleRepository;
+    private final TimerService timerService;
+    private final StartDataTransferringTask startDataTransferringTask;
+    private final EndDataTransferringTask endDataTransferringTask;
+
     @Autowired
-    ScheduleRepository scheduleRepository;
-
-    private final String endPoint = "/api/v1";
-
-    @PostMapping(value = endPoint, consumes = MediaType.APPLICATION_JSON_VALUE)
-    public void saveSchedule(@RequestBody Schedule schedule){
-        scheduleRepository.add(schedule);
+    ScheduleController(ScheduleRepository scheduleRepository, TimerService timerService,
+                       StartDataTransferringTask startDataTransferringTask,
+                       EndDataTransferringTask endDataTransferringTask) {
+        this.scheduleRepository = scheduleRepository;
+        this.timerService = timerService;
+        this.startDataTransferringTask = startDataTransferringTask;
+        this.endDataTransferringTask = endDataTransferringTask;
     }
 
+    private final String END_POINT = "/schedule";
+
+    @PostMapping(value = END_POINT, consumes = MediaType.APPLICATION_JSON_VALUE)
+    public void saveSchedule(@RequestBody Schedule schedule) {
+        Collection<TimeFrame> timeFrames = schedule.getTimeFrames();
+        TimeFrameComparatorToMin timeFrameComparatorToMin = new TimeFrameComparatorToMin();
+        Optional<TimeFrame> minTimeFrame = timeFrames.stream().min(timeFrameComparatorToMin);
+        if (minTimeFrame.isEmpty()) {
+            return;
+        }
+        LocalDateTime startTime = minTimeFrame.get().getStartTime();
+        scheduleRepository.add(schedule);
+        timerService.createTask(startDataTransferringTask, startTime);
+        TimeFrameComparatorToMax timeFrameComparatorToMax = new TimeFrameComparatorToMax();
+        Optional<TimeFrame> maxTimeFrame = timeFrames.stream().max(timeFrameComparatorToMax);
+        if (maxTimeFrame.isEmpty()) {
+            return;
+        }
+        LocalDateTime endTime = maxTimeFrame.get().getEndTime();
+        timerService.createTask(endDataTransferringTask, endTime);
+    }
 
 
 }
